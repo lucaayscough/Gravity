eb00f7b399d5f2a2ade6fbd5fe26be5d8f0bfda5
Mishell <lucaayscough@gmail.com>
2021-05-07 11:38:53 +0200

    Torchscript conversion added.

---
+++ Generator/script_builder.py
@@ -0,0 +1,33 @@
+import torch
+from script_model import Generator
+
+
+# Instantiate model
+netG = Generator(
+    z_dim = 512,
+    nf = 512,
+    kernel_size = 9,
+    stride = 1,
+    padding = 4,
+    dilation = 1,
+    depth = 7,
+    num_channels = 1,
+    scale_factor = 4,
+    use_linear_upsampling = False,
+    use_self_attention = False,
+    use_pixel_norm = False,
+    use_instance_norm = True,
+    use_style = True,
+    use_noise = True,
+    start_size = 32
+)
+
+
+# Load model.
+checkpointG = torch.load("runs/iter_2/model/checkpoint_014_G.pth.tar")
+netG.load_state_dict(checkpointG['state_dict'])
+
+
+# Serialization.
+script_module = torch.jit.script(netG)
+script_module.save("traced_generator.pt")
--- Generator/script_model.py
+++ Generator/script_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn.functional import interpolate
+import torch.nn.functional as F
 import numpy as np
 import random
 import math
@@ -88,8 +89,8 @@ class ApplyNoise(nn.Module):
         super().__init__()
         self.weight = nn.Parameter(torch.zeros(channels))
 
-    def forward(self, x, noise = None):
-        if noise == None:
+    def forward(self, x, noise):
+        if noise == torch.tensor(0):
             noise = torch.randn(x.size(0), 1, x.size(2), device = x.device, dtype = x.dtype)
         x = x + self.weight.view(1, -1, 1) * noise
         return x
@@ -122,67 +123,6 @@ class PixelNorm(nn.Module):
     def forward(self, x, epsilon: float = 1e-8):
         x = x / (x.pow(2.0).mean(dim = 1, keepdim = True).add(epsilon).sqrt())
         return x
-
-# ------------------------------------------------------------
-# Minibatch standard deviation layer for discriminator. Used to increase diversity in generator.
-
-class MiniBatchStdDev(nn.Module):
-    """
-    Minibatch standard deviation layer for the discriminator
-    Args:
-        group_size: Size of each group into which the batch is split
-    """
-
-    def __init__(self, group_size = 4) -> None:
-        """
-        Args:
-            group_size: Size of each group into which the batch is split
-        """
-        super(MiniBatchStdDev, self).__init__()
-        self.group_size = group_size
-
-    def extra_repr(self) -> str:
-        return f"group_size={self.group_size}"
-
-    def forward(self, x, alpha: float = 1e-8):
-        """
-        forward pass of the layer
-        Args:
-            x: input activation volume
-            alpha: small number for numerical stability
-        Returns: y => x appended with standard deviation constant map
-        """
-        batch_size, channels, samples = x.shape
-        if batch_size > self.group_size:
-            assert batch_size % self.group_size == 0, (
-                f"batch_size {batch_size} should be "
-                f"perfectly divisible by group_size {self.group_size}"
-            )
-            group_size = self.group_size
-        else:
-            group_size = batch_size
-
-        # reshape x into a more amenable sized tensor
-        y = torch.reshape(x, [group_size, -1, channels, samples])
-
-        # indicated shapes are after performing the operation
-        # [G x M x C x H x W] Subtract mean over groups
-        y = y - y.mean(dim=0, keepdim=True)
-
-        # [M x C x H x W] Calc standard deviation over the groups
-        y = torch.sqrt(y.square().mean(dim=0, keepdim=False) + alpha)
-
-        # [M x 1 x 1 x 1]  Take average over feature_maps and pixels.
-        y = y.mean(dim=[1, 2], keepdim=True)
-
-        # [B x 1 x H x W]  Replicate over group and pixels
-        y = y.repeat(group_size, 1, samples)
-
-        # [B x (C + 1) x H x W]  Append as new feature_map.
-        y = torch.cat([x, y], 1)
-
-        # return the computed values:
-        return y
 
 # ------------------------------------------------------------
 # Self-attention layer.
@@ -242,14 +182,11 @@ class LayerEpilogue(nn.Module):
         
         self.l_relu = nn.LeakyReLU(0.2)
     
-    def forward(self, x, latent_w, noise = None):
+    def forward(self, x, latent_w, noise):
         if self.use_noise:
             self.apply_noise(x, noise)
         
         x = self.l_relu(x)
-
-        if self.use_pixel_norm:
-            x = self.pixel_norm(x)
 
         if self.use_instance_norm:
             x = self.instance_norm(x)
@@ -310,147 +247,16 @@ class GenGeneralConvBlock(nn.Module):
         if self.use_self_attention:
             self.self_attention = SelfAttention(in_channels)
     
-    def forward(self, x, latent_w, noise = None):
-        if self.use_linear_upsampling:
-            x = self.linear_upsampling(x)
-        else:
-            x = interpolate(x, scale_factor = self.scale_factor)
+    def forward(self, x, latent_w, noise):
+        x = interpolate(x, scale_factor = self.scale_factor)
 
         # First block
         x = self.conv_block_1(x)
         x = self.layer_epilogue_1(x, latent_w[:, 0], noise)
         
-        # Self attention module
-        if self.use_self_attention:
-            x = self.self_attention(x)
-
         # Second block
         x = self.conv_block_2(x)
         x = self.layer_epilogue_2(x, latent_w[:, 1], noise)
-        return x
-
-# ------------------------------------------------------------
-# General discriminator block.
-
-class DisGeneralConvBlock(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size,
-        stride,
-        padding,
-        dilation,
-        scale_factor,
-        use_linear_downsampling,
-        use_self_attention,
-        bias = True
-    ):
-        super().__init__()
-        
-        self.use_self_attention = use_self_attention
-
-        self.conv_block_1 = EqualizedConv1d(in_channels = in_channels, out_channels = in_channels, kernel_size = kernel_size, stride = stride, padding = padding, dilation = dilation, bias = bias)
-        self.conv_block_2 = EqualizedConv1d(in_channels = in_channels, out_channels = out_channels, kernel_size = kernel_size, stride = stride, padding = padding, dilation = dilation, bias = bias)
-
-        if self.use_self_attention:
-            self.self_attention = SelfAttention(in_channels)
-
-        if use_linear_downsampling:
-            self.down_sampler = nn.Upsample(scale_factor = 1 / scale_factor, mode = 'linear')
-        else:
-            self.down_sampler = nn.AvgPool1d(scale_factor)
-        
-        self.l_relu = nn.LeakyReLU(0.2)
-    
-    def forward(self, x):
-        x = self.l_relu(self.conv_block_1(x))
-
-        if self.use_self_attention:
-            x = self.self_attention(x)
-
-        x = self.l_relu(self.conv_block_2(x))
-        x = self.down_sampler(x)
-        return x
-
-# ------------------------------------------------------------
-# Final discriminator block.
-
-class DisFinalConvBlock(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        num_channels,
-        kernel_size,
-        stride,
-        padding,
-        dilation,
-        scale_factor,
-        use_linear_downsampling,
-        use_self_attention,
-        bias = True
-    ):
-        super().__init__()
-
-        self.use_self_attention = use_self_attention
-        
-        in_channels += 1
-        out_channels += 1
-
-        self.mini_batch = MiniBatchStdDev()
-
-        self.conv_block_1 = EqualizedConv1d(
-            in_channels = in_channels,
-            out_channels = in_channels,
-            kernel_size = kernel_size,
-            stride = stride,
-            padding = padding,
-            dilation = dilation,
-            bias = bias
-        )
-
-        self.conv_block_2 = EqualizedConv1d(
-            in_channels = in_channels,
-            out_channels = in_channels,
-            kernel_size = kernel_size,
-            stride = stride,
-            padding = padding,
-            dilation = dilation,
-            bias = bias
-        )
-
-        self.conv_block_3 = EqualizedConv1d(
-            in_channels = in_channels,
-            out_channels = out_channels,
-            kernel_size = 1,
-            stride = 1,
-            padding = 0,
-            bias = bias
-        )
-
-        self.l_relu = nn.LeakyReLU(0.2)
-
-        if self.use_self_attention:
-            self.self_attention = SelfAttention(in_channels)
-        
-        if use_linear_downsampling:
-            self.down_sampler = nn.Upsample(scale_factor = 1 / scale_factor, mode = 'linear')
-        else:
-            self.down_sampler = nn.AvgPool1d(scale_factor)
-    
-    def forward(self, x):
-        x = self.mini_batch(x)
-        x = self.l_relu(self.conv_block_1(x))
-
-        if self.use_self_attention:
-            x = self.self_attention(x)
-
-        x = self.l_relu(self.conv_block_2(x))
-        x = self.conv_block_3(x)
-
-        x = self.down_sampler(x)
-
         return x
 
 # ------------------------------------------------------------
@@ -463,7 +269,7 @@ class ConstantInput(nn.Module):
         self.constant_input = nn.Parameter(torch.randn(1, nf, start_size))
         self.bias = nn.Parameter(torch.zeros(nf))
     
-    def forward(self, batch_size):
+    def forward(self, batch_size: int):
         x = self.constant_input.expand(batch_size, -1, -1)
         x = x + self.bias.view(1, -1, 1)
         return x
@@ -590,7 +396,7 @@ class Generator(nn.Module):
                     stride = stride,
                     padding = padding,
                     dilation = dilation,
-                    scale_factor = self.scale_factor,
+                    scale_factor = float(self.scale_factor),
                     use_pixel_norm = use_pixel_norm,
                     use_instance_norm = use_instance_norm,
                     use_style = use_style,
@@ -615,149 +421,54 @@ class Generator(nn.Module):
     def forward(
         self,
         latent_z,
-        is_training = True,
-        latent_w = None,
-        noise = None
+        noise = torch.tensor(0) 
     ):     
-        if is_training:
-            x = self._train(latent_z)
-            return x
-        else:
-            x = self._generate(latent_z)
-            return x
-        
-    def _generate(self, latent_z):
-        pass
-
-    def _train(self, latent_z):
-        batch_size = latent_z.size(0)
+        batch_size: int  = int(latent_z.size(0))
 
         x = self.constant_input(batch_size)
-        latent_w = self.mapping_network(latent_z)
         
-        # Style mixing and truncation.
-        self.truncation.update(latent_w[0, 0].detach())
-        latent_w = self._mixing_regularization(latent_z, latent_w, self.depth)
+        latent_w = self.mapping_network(latent_z)
         latent_w = self.truncation(latent_w)
         
-        i = 0
-        for layer_block in self.layers[: self.depth]: 
-            x = layer_block(x, latent_w[:, 2 * i : 2 * i + 2])
-            skip = self.converters[i](x)
-
-            if i == 0:
-                out = skip
-            else:
-                out = nn.Upsample(scale_factor = self.scale_factor, mode = "linear")(out)
-                out = out + skip
-            i += 1
-
+        # Layer 1
+        x = self.layers[0](x, latent_w[:, 0:2], noise)
+        skip = self.converters[0](x)
+        out = skip
+                 
+        # Layer 2
+        x = self.layers[1](x, latent_w[:, 2:4], noise) 
+        skip = self.converters[1](x)
+        out = F.upsample(out, scale_factor = float(self.scale_factor), mode = "linear")
+        out = out + skip
+        
+        # Layer 3
+        x = self.layers[2](x, latent_w[:, 4:6], noise) 
+        out = F.upsample(out, scale_factor = float(self.scale_factor), mode = "linear")
+        skip = self.converters[2](x)
+        out = out + skip
+        
+        # Layer 4
+        x = self.layers[3](x, latent_w[:, 6:8], noise)
+        out = F.upsample(out, scale_factor = float(self.scale_factor), mode = "linear")
+        skip = self.converters[3](x)
+        out = out + skip
+        
+        # Layer 5
+        x = self.layers[4](x, latent_w[:, 8:10], noise)
+        out = F.upsample(out, scale_factor = float(self.scale_factor), mode = "linear")
+        skip = self.converters[4](x)
+        out = out + skip
+        
+        # Layer 6
+        x = self.layers[5](x, latent_w[:, 10:12], noise)
+        out = F.upsample(out, scale_factor = float(self.scale_factor), mode = "linear")
+        skip = self.converters[5](x)
+        out = out + skip
+        
+        # Layer 7
+        x = self.layers[6](x, latent_w[:, 12:14], noise)
+        out = F.upsample(out, scale_factor = float(self.scale_factor), mode = "linear")
+        skip = self.converters[6](x)
+        out = out + skip
+         
         return out
-
-    def _mixing_regularization(self, latent_z, latent_w, depth):
-        latent_z_2 = torch.randn(latent_z.shape).to(latent_z.device)
-        latent_w_2 = self.mapping_network(latent_z_2)
-
-        layer_idx = torch.from_numpy(np.arange(self.depth * 2)[np.newaxis, :, np.newaxis]).to(latent_z.device)
-        cur_layers = 2 * (depth + 1)
-
-        mixing_cutoff = random.randint(1, depth + 1) if random.random() < 0.9 else cur_layers
-        latent_w = torch.where(layer_idx < mixing_cutoff, latent_w, latent_w_2)
-        return latent_w
-
-# ------------------------------------------------------------
-# Discriminator network.
-
-class Discriminator(nn.Module):
-    def __init__(self,
-        nf,
-        kernel_size,
-        stride,
-        padding,
-        dilation,
-        depth,
-        num_channels,
-        scale_factor,
-        start_size,
-        use_linear_downsampling,
-        use_self_attention
-    ):
-        super().__init__()
-        
-        self.nf = nf
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
-        self.depth = depth
-        self.num_channels = num_channels
-        self.scale_factor = scale_factor
-        self.start_size = start_size
-        self.use_linear_downsampling = use_linear_downsampling
-        self.use_self_attention = use_self_attention
-
-        # Main discriminator convolution blocks.
-        self.layers = nn.ModuleList([])
-        
-        n = self.nf
-        for l in range(depth - 1):
-            self.layers.append(
-                DisGeneralConvBlock(
-                    in_channels = n,
-                    out_channels = n * 2,
-                    kernel_size = self.kernel_size,
-                    stride = self.stride,
-                    padding = self.padding,
-                    dilation = self.dilation,
-                    scale_factor = self.scale_factor,
-                    use_linear_downsampling = self.use_linear_downsampling,
-                    use_self_attention = self.use_self_attention
-                )
-            )
-            n = n * 2
-        
-        # Final discriminator convolution block.
-        self.layers.append(
-            DisFinalConvBlock(
-                in_channels = n,
-                out_channels = n * 2,
-                num_channels = self.num_channels,
-                kernel_size = self.kernel_size,
-                stride = self.stride,
-                padding = self.padding,
-                dilation = self.dilation,
-                scale_factor = self.start_size,
-                use_linear_downsampling = self.use_linear_downsampling,
-                use_self_attention = self.use_self_attention
-            )
-        )
-        
-        # List of converters that broadcast channels from "num_channels" to "n".
-        self.converters = EqualizedConv1d(num_channels, self.nf, 1, 1, 0)
-
-        self.res_converters = nn.ModuleList([])
-        n = self.nf
-        for l in range(self.depth):
-            if l == self.depth - 1:
-                self.res_converters.append(EqualizedConv1d(n, n * 2 + 1, 1, 1, 0))
-            else:
-                self.res_converters.append(EqualizedConv1d(n, n * 2, 1, 1, 0))
-            n = n * 2
-
-        self.linear = EqualizedLinear(n + 1, num_channels)
-
-
-    def forward(self, x):
-        x = self.converters(x)
-
-        for i in range(self.depth):
-            if i < self.depth - 1:
-                residual = self.res_converters[i](nn.Upsample(scale_factor = 1 / self.scale_factor,  mode = "linear")(x))
-            else:
-                residual = self.res_converters[i](nn.Upsample(scale_factor = 1 / self.start_size,  mode = "linear")(x))
-            x = self.layers[i](x)
-            x = (x + residual) * (1 / math.sqrt(2))
-        
-        x = self.linear(x.squeeze(2))
-
-        return x
